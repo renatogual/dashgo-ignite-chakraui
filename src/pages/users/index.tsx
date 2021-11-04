b30d1e7122ed94ed662ca050e9aecc17756a7343
renatogual <renatogual@hotmail.com>
2021-11-04 09:42:19 -0300

    feat: add fetching into users list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,7 @@ import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
+  const { data, isLoading, error, isFetching } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
@@ -28,6 +28,8 @@ export default function UserList() {
     }))
 
     return users
+  }, {
+    staleTime: 1000 * 5 //5 segundos para não refazer a requisição ao receber foco na tela
   })
 
   return (
@@ -39,7 +41,11 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+            </Heading>
 
             <Link href="/users/create" passHref>
               <Button
